d172964bfc81524026cb34cf3c7b77df46efa213
Joe Brooksbank <jbrooksbank17@moundsparkacademy.org>
2016-10-10 12:02:06 -0500

    added man page

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ if [ "$(whoami)" != "root" ]; then
 			exit 1
 		fi
 cp  $PWD/xkcd_download /usr/bin/xkcd_download 
+cp $PWD/xkcd_download.1.gz /usr/share/man/man1
 echo "Sucessfully installed, images located in ~/.xkcd_download (run xkcd_download to initialize)"
+
--- remove.sh
+++ remove.sh
@@ -2,3 +2,4 @@
 
 rm -R ~/.xkcd_download 
 sudo rm /usr/bin/xkcd_download
+sudo rm /usr/share/man/man1/xkcd_download.1.gz
