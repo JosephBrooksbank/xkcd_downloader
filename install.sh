15077f94253447062dcef917fd1ca94362ee938b
Joe Brooksbank <jbrooksbank17@moundsparkacademy.org>
2016-10-11 22:40:34 -0500

    ...fixed install script syntax errors

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [ "$(whoami)" != "root" ]; then
 		exit 1
 	fi
 
-if !which convert > /dev/null; then 
+	if ! (which convert > /dev/null); then 
 	echo "ImageMagick not installed, attepting to install..."
 	if [[ "$OSTYPE" == 'linux-gnu' ]]; then
 		echo "Too many options for Linux installs, please install ImageMagick yourself" 
@@ -14,13 +14,13 @@ if !which convert > /dev/null; then
 		fi
 	if [[ $OSTYPE == darwin* ]]; then 
 		echo "Using Mac OSX, continuing using HomeBrew..."
-		if !which brew > /dev/null; then 
+		if ! (which brew > /dev/null); then 
 			echo "please install homebrew to continue"
 			echo "if you use MacPorts, please install ImageMagick manually"
 			exit 1 
 		fi 
 		sudo brew install ImageMagick
-		if !which convert > /dev/null; then
+		if ! (which convert > /dev/null); then
 			echo "install failed, try to install ImageMagick manually"
 			exit 1
 		fi
