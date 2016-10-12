57ea4c567b94447810b4526b6fa3e73a07b44146
Joe Brooksbank <jbrooksbank17@moundsparkacademy.org>
2016-10-12 14:00:42 -0500

    fixed install script (again), added mac version

--- install.sh
+++ install.sh
@@ -1,33 +1,36 @@
 #!/bin/bash 
 
+osType=$(uname -s)
 
 if [ "$(whoami)" != "root" ]; then
 	echo "Sorry, you are not root."
 		exit 1
 	fi
 
-	if ! (which convert > /dev/null); then 
-	echo "ImageMagick not installed, attepting to install..."
-	if [[ "$OSTYPE" == 'linux-gnu' ]]; then
-		echo "Too many options for Linux installs, please install ImageMagick yourself" 
+if type convert 2>/dev/null; then
+	case "$osType" in 
+		"Darwin")
+		{
+			echo "Installing mac version..."
+			cp $PWD/xkcd_download-mac /usr/bin/xkcd_download
+			cp $PWD/xkcd_download.1.gz /usr/share/man/man1
+			echo "Successfully installed, images located in ~/.xkcd_download (run xkcd_download to intialize)"
+		} ;;
+		"Linux")
+		{
+			echo "Installing LINUX  version..."
+			cp  $PWD/xkcd_download /usr/bin/xkcd_download 
+			cp $PWD/xkcd_download.1.gz /usr/share/man/man1
+			echo "Sucessfully installed, images located in ~/.xkcd_download (run xkcd_download to initialize)"
+			exit 0
+		} ;;
+		*)
+		{
+			echo "Unknown OS, exiting"
 			exit 1
-		fi
-	if [[ $OSTYPE == darwin* ]]; then 
-		echo "Using Mac OSX, continuing using HomeBrew..."
-		if ! (which brew > /dev/null); then 
-			echo "please install homebrew to continue"
-			echo "if you use MacPorts, please install ImageMagick manually"
-			exit 1 
-		fi 
-		sudo brew install ImageMagick
-		if ! (which convert > /dev/null); then
-			echo "install failed, try to install ImageMagick manually"
-			exit 1
-		fi
-	fi 
+		} ;;
+	esac
+else
+	echo "ImageMagick not installed, exiting..."
+	exit 1
 fi
-
-cp  $PWD/xkcd_download /usr/bin/xkcd_download 
-cp $PWD/xkcd_download.1.gz /usr/share/man/man1
-echo "Sucessfully installed, images located in ~/.xkcd_download (run xkcd_download to initialize)"
-
