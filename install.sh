c9b8e56ce2933bddd7a5efdc73944651c2c32eff
Brookshank <jbrooksbank17@moundsparkacademy.org>
2016-11-24 22:10:52 -0600

    Reworked Entire Script
    Re wrote code, added functions
    
    removed need for separate mac and linux versions
    
    simplified install script
    
    added config file
    
    default locations for install is ~/.xkcd_download, config file is found
    at ~/.config/xkcd_download/xkcd_download.conf

--- install.sh
+++ install.sh
@@ -1,36 +1,10 @@
 #!/bin/bash 
 
-osType=$(uname -s)
-
-if [ "$(whoami)" != "root" ]; then
-	echo "Sorry, you are not root."
-		exit 1
-	fi
-
-if type convert 2>/dev/null; then
-	case "$osType" in 
-		"Darwin")
-		{
-			echo "Installing mac version..."
-			cp $PWD/xkcd_download-mac /usr/bin/xkcd_download
-			cp $PWD/xkcd_download.1.gz /usr/share/man/man1
-			echo "Successfully installed, images located in ~/.xkcd_download (run xkcd_download to intialize)"
-		} ;;
-		"Linux")
-		{
-			echo "Installing LINUX  version..."
-			cp  $PWD/xkcd_download /usr/bin/xkcd_download 
-			cp $PWD/xkcd_download.1.gz /usr/share/man/man1
-			echo "Sucessfully installed, images located in ~/.xkcd_download (run xkcd_download to initialize)"
-			exit 0
-		} ;;
-		*)
-		{
-			echo "Unknown OS, exiting"
-			exit 1
-		} ;;
-	esac
-else
-	echo "ImageMagick not installed, exiting..."
-	exit 1
-fi
+#if [ "$(whoami)" != "root" ]; then
+#	echo "Sorry, you are not root."
+#		exit 1
+#	fi
+cp  $PWD/xkcd_download /usr/bin/xkcd_download
+sudo cp $PWD/xkcd_download.1.gz /usr/share/man/man1
+mkdir $HOME/.config/xkcd_download
+cp $PWD/xkcd_download.conf $HOME/.config/xkcd_download
