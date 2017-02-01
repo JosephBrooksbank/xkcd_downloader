5f4c7951c0e2f330d46c4d096faf3feacc4e947b
Brookshank <jbrooksbank17@moundsparkacademy.org>
2017-02-01 10:30:26 -0600

    Edited install script to check for sudo and not overwrite config file if it already exists. Images now resize to a screen friendly size if too large

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
-#if [ "$(whoami)" != "root" ]; then
-#	echo "Sorry, you are not root."
-#		exit 1
-#	fi
+[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
 cp  $PWD/xkcd_download /usr/bin/xkcd_download
 sudo cp $PWD/xkcd_download.1.gz /usr/share/man/man1
-mkdir $HOME/.config/xkcd_download
-cp $PWD/xkcd_download.conf $HOME/.config/xkcd_download
+
+if [ ! -d "$HOME/.config/xkcd_download" ]; then
+    mkdir $HOME/.config/xkcd_download
+    cp $PWD/xkcd_download.conf $HOME/.config/xkcd_download
+fi
